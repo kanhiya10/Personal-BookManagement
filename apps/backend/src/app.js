6595908a478b43193c166c054a3c6647a277f4a9
kanhiya10 <kanhiyagandhi002@gmail.com>
2026-08-03 22:32:46 +0530

    changes v2

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['https://bookmanagement-git-main-kanhiya-s-projects.vercel.app'],
+    origin: ['https://bookmanagement-giohqmhhz-kanhiya-s-projects.vercel.app'],
     credentials:true,
 }))
 
