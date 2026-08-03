8055e22ca76a670d395ac75f854e249725af6287
kanhiya10 <kanhiyagandhi002@gmail.com>
2026-08-03 21:42:27 +0530

    set the prod. backend url

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://bookmanagement-a3nk0jax9-kanhiya-s-projects.vercel.app/'],
     credentials:true,
 }))
 
