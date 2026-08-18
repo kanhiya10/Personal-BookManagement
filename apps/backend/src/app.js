e43f9c61c6d591fb7464115d18e9ee5b105b4a4a
kanhiya10 <kanhiyagandhi002@gmail.com>
2026-08-19 01:09:21 +0530

    fix the add book fn

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['https://bookmanagement-giohqmhhz-kanhiya-s-projects.vercel.app','http://localhost:3000'],
+    origin: ['https://bookmanagement-navy-chi.vercel.app'],
     credentials:true,
 }))
 
